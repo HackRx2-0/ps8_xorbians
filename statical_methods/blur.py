8649cd1c47821d7643eb060aef61b50fea5d91cf
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2021-07-23 13:00:17 +0530

    blurring check

---
+++ statical_methods/blur.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+def check_blur(img):
+	sobelxy = cv2.Sobel(src=img, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)
+	if np.std(sobelxy)<50:
+		blur = True
+	else:
+		blur = False
+	return sobelxy, blur
