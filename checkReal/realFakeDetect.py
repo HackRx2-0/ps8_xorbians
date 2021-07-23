17b88991ed3f69b4776df18cfbe0e1aa0afe209e
Vedant Rishi Das <vedantdas130701@gmail.com>
2021-07-23 16:14:23 +0530

    checking if image is real/fake

---
+++ checkReal/realFakeDetect.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+import pickle
+import cv2
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+model = load_model("./checkReal/realFake.model")
+
+def check_real(image):
+    # image = cv2.imread(img)
+    image = cv2.resize(image, (32, 32))
+    image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+    data = []
+    data.append(image)
+    data = np.array(data, dtype="float") / 255.0
+
+    prediction = model.predict(x=data)
+    if prediction[0][1] > prediction[0][0]:
+        print(prediction[0][1])
+        return False
+    print(prediction[0][0])
+    return True
