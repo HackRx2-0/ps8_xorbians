237576ab296572e69d467de80d264d5a8f3d1534
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2021-07-23 15:08:10 +0530

    size resize

---
+++ statical_methods/size.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+def check_dimensions(orig, img, base_size=0.2, lim=10000):
+	area_orig = orig.shape[0]*orig.shape[1]
+	area_face = img.shape[0]*img.shape[1]
+	if (area_face/area_orig) < base_size and area_face<lim:
+		small = True
+		scale_percent = (area_orig/area_face)**(0.5)
+		width = int(img.shape[1] * scale_percent)
+		height = int(img.shape[0] * scale_percent)
+		dim = (width, height)
+		resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+	else:
+		small = False
+		resized = img
+	return resized, small
